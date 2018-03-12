18caca9b5049c0bfe45b49deaae5f2dced6de77a
luisdramirez <ldr285@nyu.edu>
2018-03-12 13:01:13 -0400

    initial push.

---
+++ vWMd_init.m
@@ -0,0 +1,7 @@
+%%% The Initializer:
+    % Set parameters, options
+    % Load data
+disp('Entering Initializer.')
+
+%%
+disp('Exiting Initializer.')
---
+++ vWMd_master.m
@@ -0,0 +1,21 @@
+%%% fMRI Contrast Classifier
+clear all; close all; clc;
+disp('Starting Contrast Classification')
+%% Initializer
+% The Initializer:
+    % Set parameters, options
+    % Load data
+    
+%% Organize data
+
+%% Create trial labels
+
+%% Convert trial labels to characters
+
+%% Accuracy score matrices
+
+%% Classification & Accuracy
+
+%% Plot
+
+ 
